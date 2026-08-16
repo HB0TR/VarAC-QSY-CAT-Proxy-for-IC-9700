82aae07c9b40ec78df6820fd69a572e158e68cc8
Stefan <45574713+HB0TR@users.noreply.github.com>
2026-08-16 23:46:26 +0200

    V5.01: update launcher title

--- Start_VarAC_QSY-CAT_Proxy.bat
+++ Start_VarAC_QSY-CAT_Proxy.bat
@@ -1,5 +1,5 @@
 @echo off
-title VarAC QSY-CAT Proxy for IC-9700 by HB0TR V5.00
+title VarAC QSY-CAT Proxy for IC-9700 by HB0TR V5.01
 cd /d "%~dp0"
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0VarAC_QSY-CAT_Proxy_IC-9700_HB0TR.ps1"
 echo.
